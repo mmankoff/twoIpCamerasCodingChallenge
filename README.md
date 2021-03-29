84b73bd14226573e2a045d84d1f50772af0c4f44
MM <75466028+mmwbsdev@users.noreply.github.com>
2021-03-28 21:35:57 -0700

    Completed Code to Pass All Possible Solutions:
    This includes the README file explaining the steps taken, and the completed code with passing tests is also added.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-two-ip-cameras-coding-challenge
+
+------------------------------------------------------------------------------------------------------------------
+     Two Ip Cameras Challenge
+-------------------------------------------------------------------------------------------------------------------
+Goal: Complete the task list:
+1) 3 seconds per test
+2) Standard input
+3) Standard Output
+4) Successfully ran using only 256 megabytes.
+---------------------------------------------------------------------------------------------------------------------
+Context:
+1) There are 2 cameras both of the same model and both can take photos beginning at some fixed point in time.
+   ( Me thinking out loud -> Inlining a rate limiter might work or a window slide recursive check.)
+2) Start time of those processes is up for me to choose but must be from a value set by the cameras
+   manufacturer which are set as one of (k) values == to (p1, p2,....,pk).
+3) You have (n) points that you are interested in == to (x1,x2,....,xn).
+4) Configure both cameras in a way that at least one of them will take a photo in each of these moments.
+   p1-> p2...pk where (k) is equal to the manufactures set start value.
+   x1 -> x2 -> x3 -> x4
+5) To configure each camera means: -> ( Setting the points where each camera takes the first photo along with
+   the gap between it's next two consecutive points where one of those points should be equal to one of the values
+   between.) (p1,p2,....,pk).
+
+6) The ability for both cameras being able to take pictures at different moments doesn't effect my points of
+   interest
+7) The application should return " This is impossible " if there if it can't find a way to cover all required points.
+   -> Printed to output.
+   .
+   Key -> The main goal is to find each point sufficient enough that both cameras start points are set so that the entire
+   distance from each of their start points to the end covers every initial point within the range it needs to
+   search.
+---------------------------------------------------------------------------------------------------------------------
+
--- src/main/kotlin/codingChallenges/TwoIpCameras.kt
+++ src/main/kotlin/codingChallenges/TwoIpCameras.kt
@@ -1,37 +1,85 @@
 package codingChallenges
+/** ------------------------------------------------------------------------------------------------------------------
+   First step -> narrow your search down to the inputs provided that will cover your base case and match
+   all points as being covered.
+       -> Start by looking at the (0,1,2) position and consider all divisor for the moments in time that
+         cover the entire time period where at least the differences of each cover the full area
+         where one camera (0) -> (1,4,5) excluding (7,12) and replacing
+  it with (0,1,2).
+----------------------------------------------------------------------------------------------------------------------
+  This will give us (0,1,2,3,4,5) where (0) is covering the start index and allowing for (1,4) to match the desired
+      output of(0) covering (1) -> (4) ||   (0) covering (2) -> (3) will provide (0,1,2,3,4,5).
 
-/**
-*------------------------------------------------------------------------------------------------------------------
-*     Two Ip Cameras Challenge
-* -------------------------------------------------------------------------------------------------------------------
-   Goal: Complete the task list:
-      1) 3 seconds per test
-      2) Standard input
-      3) Standard Output
-      4) Successfully ran using only 256 megabytes.
- ---------------------------------------------------------------------------------------------------------------------
-    Context:
-    1) There are 2 cameras both of the same model and both can take photos beginning at some fixed point in time.
-        ( Me thinking out loud -> Inlining a rate limiter might work or a window slide recursive check.)
-    2) Start time of those processes is up for me to choose but must be from a value set by the cameras
-        manufacturer which are set as one of (k) values == to (p1, p2,....,pk).
-    3) You have (n) points that you are interested in == to (x1,x2,....,xn).
-    4) Configure both cameras in a way that at least one of them will take a photo in each of these moments.
-                p1-> p2...pk where (k) is equal to the manufactures set start value.
-         x1 -> x2 -> x3 -> x4
-    5) To configure each camera means: -> ( Setting the points where each camera takes the first photo along with
-      the gap between it's next two consecutive points where one of those points should be equal to one of the values
-      between.) (p1,p2,....,pk).
-
-    6) The ability for both cameras being able to take pictures at different moments doesn't effect my points of
-     interest
-.
-Key -> The main goal is to find each point sufficient enough that both cameras start points are set so that the entire
-     distance from each of their start points to the end covers every initial point within the range it needs to
-     search.
- ---------------------------------------------------------------------------------------------------------------------
+  The period of the camera should be a divisor of the difference of both moments in time
+     Example inputs:
+       Camera 1
+        (3,5)
+       (3,5,7)
+    (1,4,5,7,12) -> / (0,1,2)
+----------------------------------------------------------------------------------------------------------------------
+    Consider these first as this provides a base case for all COVERED moments in time
+    Camera 2
+     (3,2)
+     (1,2,3)
+     (1,10)
+--------------------------------------------------------------------------------------------------------------------
+    Outputs:
+     YES
+     (1,3)
+     (5,7)
+--------------------------------------------------------------------------------------------------------------------
  */
+// First I want to iterate over (k)
+// To ensure edge cases where a larger amount of inputs needs to be searched through, I need to add a limitor
+// here that will reduce the problem and allow me to not worry about receiving too large of an input to search. I
+// want to take the smallest possible number to search through for the input to start.
+fun solve(periods: List<Int>, moments: List<Int>): List<Int>? {
 
-fun main(){
+    val goodStartingIndex = (0..moments.size - 3).minByOrNull{ moments[it + 2] - moments[it] }
+                    ?: return listOf( moments[0], periods[0],moments[1] ,periods[0])
+    val triple = goodStartingIndex..goodStartingIndex + 2
+    for (k in triple) {
+        val (i, j) = triple.minus(k)
+        for (period1 in periods) {
+            val remainderI = moments[i] % period1
+            val remainderJ = moments[j] % period1
+            if (remainderI != remainderJ) continue
+            val uncoveredMoments = moments.filter { it % period1 != remainderI }
 
+            //defining the gcd set to 0 is ok to do and will be the start here which gets updated with all the new
+            // GCD for each difference. Using a fold function would work here.
+            val gcdOfDifferences = uncoveredMoments.zipWithNext(Int::minus).fold(0, ::gcd)
+
+            // linear time still and this will provide the
+            val period2 = periods.firstOrNull{ gcdOfDifferences % it == 0} ?: continue
+            val phase1 = phase(remainderI, period1)
+            val phase2 = phase(uncoveredMoments.firstOrNull() ?: 0 , period2)
+            return listOf(phase1,period1,phase2,period2)
+            // Setting both phases so as they will cover both moments where my index points match moments i and j.
+        }
+    }
+    return null
 }
+
+/**------------------------------------------------------------------------------------------------------------------
+ I'm going to be using a tailrec recursive tail function.
+ This will be the fastest solution but I've demonstrated 2 other examples of how it can be done as well with slower
+ results. -> 1) return a.toBigInteger().gcd(b.toBigInteger().toInt()) or
+             2)return a.toBigInteger().gcd(b.toBigInteger().toInt())
+ --------------------------------------------------------------------------------------------------------------------
+*/
+private fun phase(remainder: Int, period: Int) = (remainder + period - 1) % period + 1
+tailrec fun gcd(a: Int, b: Int): Int = if (a == 0) b else gcd(b % a, a)
+fun main(){
+    readLine()
+    val periods = readInts()
+    val moments = readInts()
+    //This will create the function that will read the entire line that aren't matched already by the other periods.
+    //To solve this I will need to add a nullable type.
+    val (phase1, period1, phase2, period2) = solve(periods,moments)
+        ?: return println("NO")
+    println("YES\n$phase1 $period1\n$phase2 $period2")
+}
+
+
+private fun readInts() = readLine()!!.split(" ").map { it.toInt() }
